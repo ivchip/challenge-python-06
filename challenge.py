37fc7b68a1c4f0ad326a9f0ed8c9203eec310223
Iván Chipatecua <ivchip@gmail.com>
2020-04-25 18:01:41 -0500

    Challenge python 06 fixed

--- challenge.py
+++ challenge.py
@@ -1,6 +1,6 @@
 def make_division_by(n):
     def repeater(val):
-        return val // n
+        return val / n
     return repeater
 
 
@@ -20,15 +20,26 @@ if __name__ == '__main__':
 
     class ClosureSuite(unittest.TestCase):
         def setUp(self):
-            self.value = 18
+            self.value_18 = 18
+            self.value_100 = 100
+            self.value_54 = 54
 
-        def test_closure_make_division_by(self):
+        def test_closure_make_division_by_3(self):
             division_by_3 = make_division_by(3)
-            self.assertEqual(6, division_by_3(18))
+            self.assertEqual(6, division_by_3(self.value_18))
+
+        def test_closure_make_division_by_5(self):
+            division_by_5 = make_division_by(5)
+            self.assertEqual(20, division_by_5(self.value_100))
+
+        def test_closure_make_division_by_18(self):
+            division_by_18 = make_division_by(18)
+            self.assertEqual(3, division_by_18(self.value_54))
 
         def tearDown(self):
-            del(self.value)
-    
-    unittest.main()
+            del(self.value_18)
+            del(self.value_100)
+            del(self.value_54)
     
     run()
+    unittest.main()
